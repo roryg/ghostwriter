980239b56fd2c077aad9217505efc2cd7764ba0c
Rory Gibson <rg.rorygibson@gmail.com>
2013-09-19 15:14:33 +0400

    Initial commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,120 @@
+jQuery(function($) {
+
+    /* ============================================================ */
+    /* Scroll To Top */
+    /* ============================================================ */
+
+    $('.js-jump-top').on('click', function(e) {
+        e.preventDefault();
+
+        $('body').animate({'scrollTop': 0});
+    });
+
+    /* ============================================================ */
+    /* Ajax Loading */
+    /* ============================================================ */
+
+    var History = window.History;
+    var loading = false;
+    var showIndex = false;
+    var $ajaxContainer = $('#ajax-container');
+    var $latestPost = $('#latest-post');
+    var $postIndex = $('#post-index');
+
+    // Initially hide the index and show the latest post
+    $latestPost.show();
+    $postIndex.hide();
+
+    // Show the index if the url has "page" in it (a simple
+    // way of checking if we're on a paginated page.)
+    if (window.location.pathname.indexOf('page') === 1) {
+        $latestPost.hide();
+        $postIndex.show();
+    }
+
+    // Check if history is enabled for the browser
+    if ( ! History.enabled) {
+        return false;
+    }
+
+    History.Adapter.bind(window, 'statechange', function() {
+        var State = History.getState();
+
+        // Get the requested url and replace the current content
+        // with the loaded content
+        $.get(State.url, function(result) {
+            var $html = $(result);
+            var $newContent = $('#ajax-container', $html);
+
+            $('body').animate({'scrollTop': 0}, function() {
+                $ajaxContainer.fadeOut(500, function() {
+                    $latestPost = $('#latest-post', $newContent);
+                    $postIndex = $('#post-index', $newContent);
+
+                    if (showIndex === true) {
+                        $latestPost.hide();
+                    } else {
+                        $latestPost.show();
+                        $postIndex.hide();
+                    }
+
+                    $ajaxContainer.html($newContent);
+                    $ajaxContainer.fadeIn(500);
+
+                    NProgress.done();
+
+                    loading = false;
+                    showIndex = false;
+                });
+            });
+        });
+    });
+    
+    $('body').on('click', '.js-ajax-link, .pagination a', function(e) {
+        e.preventDefault();
+
+        if (loading === false) {
+            var currentState = History.getState();
+            var url = $(this).attr('href');
+            var title = $(this).attr('title') || null;
+
+            // If the requested url is not the current states url push
+            // the new state and make the ajax call.
+            if (url !== currentState.url.replace(/\/$/, "")) {
+                loading = true;
+
+                // Check if we need to show the post index after we've
+                // loaded the new content
+                if ($(this).hasClass('js-show-index') || $(this).parent('.pagination').length > 0) {
+                    showIndex = true;
+                }
+
+                NProgress.start();
+
+                History.pushState({}, title, url);
+            } else {
+                // Swap in the latest post or post index as needed
+                if ($(this).hasClass('js-show-index')) {
+                    $('body').animate({'scrollTop': 0}, function() {
+                        NProgress.start();
+
+                        $latestPost.fadeOut(300, function() {
+                            $postIndex.fadeIn(300);
+                            NProgress.done();
+                        });
+                    });
+                } else {
+                    $('body').animate({'scrollTop': 0}, function() {
+                        NProgress.start();
+
+                        $postIndex.fadeOut(300, function() {
+                            $latestPost.fadeIn(300);
+                            NProgress.done();
+                        });
+                    });
+                }
+            }
+        }
+    });
+
+});
